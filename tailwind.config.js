aa6aacf7fda040a1fd174f089d2893ca378c153f
Maruf <marufabdulloev.888@mail.ru>
2024-11-25 21:14:55 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {screens: {
+      'sm': { 'max': '576px' },
+      // => @media (max-width: 576px) { ... }
+
+      'md': { 'max': '960px' },
+      // => @media (max-width: 960px) { ... }
+
+      'lg': { 'max': '1440px' },
+      // => @media (max-width: 1440px) { ... }
+    },
+  },
+  plugins: [],
+ 
+}}
+
